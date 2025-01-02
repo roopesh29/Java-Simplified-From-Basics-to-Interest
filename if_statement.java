4eefcf2712527e9ab9cdbc655c51f48da90c9096
roopesh29 <roopesh3252@gmail.com>
2025-01-02 17:22:24 +0530

    Create if_statement.java

---
+++ if_statement.java
@@ -0,0 +1,16 @@
+/*Understanding if Statements in Java"
+The if statement is a fundamental control structure in Java used to execute code based on a condition. It helps make decisions in a program.
+ *
+ */
+class IfExample {
+    public static void main(String[] args) {
+        int number = 10; // Example number
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+
+        System.out.println("Program complete.");
+
+    }
+}
